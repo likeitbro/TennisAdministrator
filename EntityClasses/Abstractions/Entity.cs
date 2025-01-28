7ff2b4a070464321b85febde61d138b09ef0dc61
Stephen <sokhrannykhs@gmail.com>
2025-01-28 15:28:50 +0500

    feat: entity abstraction added

---
+++ EntityClasses/Abstractions/Entity.cs
@@ -0,0 +1,71 @@
+﻿namespace EntityClasses.Abstractions;
+
+/// <summary>
+/// Represents some abstract entity with uknown type
+/// </summary>
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+
+    protected Entity()
+    {
+
+    }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+}
+
+/// <summary>
+/// Some entity with specific type
+/// </summary>
+/// <typeparam name="TEntity">Actual entity class</typeparam>
+public abstract class Entity<TEntity> 
+    : Entity, IEquatable<Entity<TEntity>>
+    where TEntity : Entity<TEntity>
+{
+
+    protected Entity()
+    {
+
+    }
+
+    protected Entity(Guid id)
+        : base(id)
+    {
+
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TEntity> other &&
+               this.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.Id.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"Entity instance of type {typeof(TEntity).Name} with Id: {this.Id}";
+    }
+
+    public virtual bool Equals(Entity<TEntity>? other)
+    {
+        return other.Id == this.Id;
+    }
+
+    public static bool operator ==(Entity<TEntity> first, Entity<TEntity> second)
+    {
+        return first.Equals(second);
+    }
+
+    public static bool operator !=(Entity<TEntity> first, Entity<TEntity> second)
+    {
+        return !(first == second);
+    }
+}
