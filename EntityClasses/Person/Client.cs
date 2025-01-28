7a1748e5423d3e0fe2935527da93925784aaa0c1
Stephen <sokhrannykhs@gmail.com>
2025-01-28 16:11:22 +0500

    feat: client type class added

---
+++ EntityClasses/Person/Client.cs
@@ -0,0 +1,35 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Person;
+
+public class ClientType: Entity<ClientType>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    protected ClientType(string name)
+        :base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static ClientType Create(string name)
+    {
+        return new(name);
+    }
+
+    public void Update(string name)
+    {
+        Name = name;
+    }
+}
