6d1d2a0c00dba49f3a6aa7248cf3f05f5cfe5130
Stephen <sokhrannykhs@gmail.com>
2025-01-28 15:52:38 +0500

    feat: abstract person class (Throw lib loaded)

---
+++ EntityClasses/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Person;
+
+/// <summary>
+/// Abstract class for people (adds firstname and lastname to abstract entity class)
+/// </summary>
+public abstract class Person: Entity
+{
+    private string _firstname;
+
+    public string FirstName 
+    {
+        get => _firstname;
+        protected set
+        {
+            _firstname = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    private string _lastname;
+
+    public string LastName
+    {
+        get => _lastname;
+        protected set
+        {
+            _lastname = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    protected Person(string firstname, string lastname)
+        : base(Guid.NewGuid())
+    {
+        FirstName = firstname;
+        LastName = lastname;
+    }
+}
