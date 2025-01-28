9805ea49c76f66e17bafb440cd50dee4d86334f3
Stephen <sokhrannykhs@gmail.com>
2025-01-28 16:59:56 +0500

    feat: client class added

---
+++ EntityClasses/Person/Client.cs
@@ -0,0 +1,25 @@
+﻿using Throw;
+using EntityClasses.Abstractions;
+
+namespace EntityClasses.Person;
+
+public class Client : EntityClasses.Abstractions.Person
+{
+    private Guid _clientTypeId;
+
+    public Guid ClientTypeId
+    {
+        get => _clientTypeId;
+        protected set =>
+            _clientTypeId = value.Throw()
+                .IfNull(cti => cti);
+    }
+
+    public ClientType ClientType { get; protected set; }
+
+    protected Client(string firstname, string lastname, DateOnly birthday, string phone, Guid clientTypeId)
+        :base(firstname, lastname, birthday, phone)
+    {
+        ClientTypeId = clientTypeId;
+    }
+}
