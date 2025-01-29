3875715237e28c81ceabf54f2cce10895f68a4dd
Stephen <sokhrannykhs@gmail.com>
2025-01-29 23:08:22 +0500

    feat: specialization junction class added

---
+++ EntityClasses/Specialization.cs
@@ -0,0 +1,42 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses;
+
+public class Specialization: Entity<Specialization>
+{
+    private Guid _trainerId;
+
+    public Guid TrainerId
+    {
+        get => _trainerId;
+        set => _trainerId = value.Throw()
+            .IfNull(ti => ti);
+    }
+
+    private Guid _sportId;
+
+    public Guid SportId
+    {
+        get => _sportId;
+        set => _sportId = value.Throw()
+            .IfNull(si => si);
+    }
+
+    private Specialization(Guid trainerId, Guid sportId)
+    {
+        TrainerId = trainerId;
+        SportId = sportId;
+    }
+
+    public static Specialization Create(Guid trainerId, Guid sportId)
+    {
+        return new(trainerId, sportId);
+    }
+
+    public void Update(Guid trainerId, Guid sportId)
+    {
+        TrainerId = trainerId;
+        SportId = sportId;
+    }
+}
