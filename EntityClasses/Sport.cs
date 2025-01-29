e4f366ff66ffb75554979ea23dd347e6c4d6117c
Stephen <sokhrannykhs@gmail.com>
2025-01-29 23:01:20 +0500

    feat: sport class added

---
+++ EntityClasses/Sport.cs
@@ -0,0 +1,32 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses;
+
+public class Sport: Entity<Sport>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value.Throw()
+            .IfNullOrWhiteSpace(n => n);
+    }
+
+    public Sport(string name)
+        :base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static Sport Create(string name)
+    {
+        return new(name);
+    }
+
+    public void Update(string name)
+    {
+        Name = name;
+    }
+}
