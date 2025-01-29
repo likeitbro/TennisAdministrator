18b5c9844a89ae8d13e0922a9a91f646332a9ccc
Stephen <sokhrannykhs@gmail.com>
2025-01-29 23:53:03 +0500

    feat: product class added

---
+++ EntityClasses/Sales/Product.cs
@@ -0,0 +1,56 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Sales;
+
+public class Product: Entity<Product>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value.Throw()
+            .IfNullOrWhiteSpace(n => n);
+    }
+
+    private float _price;
+
+    public float Price
+    {
+        get => _price;
+        set => _price = value.Throw()
+            .IfNull(p => p)
+            .IfNegativeOrZero(p => p);
+    }
+
+    private int _quantity;
+
+    public int Quantity
+    {
+        get => _quantity;
+        set => _quantity = value.Throw()
+            .IfNull(q => q)
+            .IfNegative(q => q);
+    }
+
+    private Product(string name, float price, int quantity)
+        :base(Guid.NewGuid())
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public static Product Create(string name, float price, int quantity)
+    {
+        return new(name, price, quantity);
+    }
+
+    public void Update(string name, float price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+}
