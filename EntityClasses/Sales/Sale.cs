d8be05c55d2dd0b2bb030cde5b942273d4d9fa83
Stephen <sokhrannykhs@gmail.com>
2025-01-30 00:03:00 +0500

    feat: sale class added

---
+++ EntityClasses/Sales/Sale.cs
@@ -0,0 +1,55 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Sales;
+
+public class Sale: Entity<Sale>
+{
+    private Guid _clientId;
+
+    public Guid ClientId
+    {
+        get => _clientId;
+        set => _clientId = value.Throw()
+            .IfNull(ci => ci);
+    }
+
+    private DateTime _saleTime;
+
+    public DateTime SaleTime
+    {
+        get => _saleTime;
+        set => _saleTime = value.Throw()
+            .IfNull(st => st);
+    }
+
+    private float _revenue;
+
+    public float Revenue
+    {
+        get => _revenue;
+        set => _revenue = value.Throw()
+            .IfNull(r => r)
+            .IfNegative(r => r);
+    }
+
+    private Sale(Guid clientId, DateTime saleTime, float revenue)
+        :base(Guid.NewGuid())
+    {
+        ClientId = clientId;
+        SaleTime = saleTime;
+        Revenue = revenue;
+    }
+
+    public static Sale Create(Guid clientId, DateTime saleTime, float revenue)
+    {
+        return new(clientId, saleTime, revenue);
+    }
+
+    public void Update(Guid clientId, DateTime saleTime, float revenue)
+    {
+        ClientId = clientId;
+        SaleTime = saleTime;
+        Revenue = revenue;
+    }
+}
