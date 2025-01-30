87540efabeff705d0408b6366e0761df1433ad43
Stephen <sokhrannykhs@gmail.com>
2025-01-30 17:48:04 +0500

    feat: tournament attendee junction class added

---
+++ EntityClasses/Tournaments/TournamentAttendee.cs
@@ -0,0 +1,43 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Tournaments;
+
+public class TournamentAttendee: Entity<TournamentAttendee>
+{
+    private Guid _clientId;
+
+    public Guid ClientId
+    {
+        get => _clientId;
+        set => _clientId = value.Throw()
+            .IfNull(ci => ci);
+    }
+
+    private Guid _tournamentId;
+
+    public Guid TournamentId
+    {
+        get => _tournamentId;
+        set => _tournamentId = value.Throw()
+            .IfNull(ti => ti);
+    }
+
+    private TournamentAttendee(Guid clientId, Guid tournamentId)
+        :base(Guid.NewGuid())
+    {
+        ClientId = clientId;
+        TournamentId = tournamentId;
+    }
+
+    public static TournamentAttendee Create(Guid clientId, Guid tournamentId)
+    {
+        return new(clientId, tournamentId);
+    }
+
+    public void Update(Guid clientId, Guid tournamentId)
+    {
+        ClientId = clientId;
+        TournamentId = tournamentId;
+    }
+}
