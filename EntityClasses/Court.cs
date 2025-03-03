6994c42b60443380ab7ec05fae59d7a519e96474
Stephen <sokhrannykhs@gmail.com>
2025-03-03 15:43:10 +0500

    feat: court class added

---
+++ EntityClasses/Court.cs
@@ -0,0 +1,61 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses;
+
+public class Court : Entity<Court>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    private string _description;
+
+    public string Description
+    {
+        get => _description;
+        protected set
+        {
+            _description = value.Throw()
+                .IfNullOrWhiteSpace(d => d);
+        }
+    }
+
+    private float _price;
+
+    public float Price
+    {
+        get => _price;
+        protected set => _price = value.Throw()
+            .IfNull(p => p)
+            .IfNegativeOrZero(p => p);
+    }
+
+    protected Court(string name, string description, float price)
+        : base(Guid.NewGuid())
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+
+    public static Court Create(string name, string description, float price)
+    {
+        return new(name, description, price);
+    }
+
+    public void Update(string name, string description, float price)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+}
