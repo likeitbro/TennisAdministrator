d84bbb0786b78e31685d7fcec7ce81ed95ea53f8
Stephen <sokhrannykhs@gmail.com>
2025-03-03 15:14:34 +0500

    feat: trainer type class added

---
+++ EntityClasses/Person/TrainerType.cs
@@ -0,0 +1,36 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Person;
+
+public class TrainerType : Entity<TrainerType>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    protected TrainerType(string name)
+        : base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static TrainerType Create(string name)
+    {
+        return new(name);
+    }
+
+    public void Update(string name)
+    {
+        Name = name;
+    }
+}
+
