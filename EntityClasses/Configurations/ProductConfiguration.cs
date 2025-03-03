550a465c9ceea8ca17fd6dd979a4bcb980c63993
Stephen <sokhrannykhs@gmail.com>
2025-03-03 22:24:12 +0500

    fix: updating to actual save

--- EntityClasses/Configurations/ProductConfiguration.cs
+++ EntityClasses/Configurations/ProductConfiguration.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EntityClasses.Sales;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace EntityClasses.Configurations
+namespace EntityClasses.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
-    internal class ProductConfiguration
+    public void Configure(EntityTypeBuilder<Product> builder)
     {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.TypeId).IsRequired();
+
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+
+        builder.Property(p => p.Price).IsRequired();
+
+        builder.Property(p => p.Quantity).IsRequired();
+
+        builder.HasOne<ProductType>(p => p.ProductType)
+            .WithMany(pt => pt.Products)
+            .HasForeignKey(p => p.TypeId)
+            .IsRequired();
     }
 }
