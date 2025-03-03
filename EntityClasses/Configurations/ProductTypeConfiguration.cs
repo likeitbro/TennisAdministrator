862bf2bc6b25c6da5136103583cc3b932c77fde7
Stephen <sokhrannykhs@gmail.com>
2025-03-03 22:02:42 +0500

    chore: wipe excessive dependencies

--- EntityClasses/Configurations/ProductTypeConfiguration.cs
+++ EntityClasses/Configurations/ProductTypeConfiguration.cs
@@ -1,12 +1,6 @@
 ﻿using EntityClasses.Sales;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntityClasses.Configurations;
 
