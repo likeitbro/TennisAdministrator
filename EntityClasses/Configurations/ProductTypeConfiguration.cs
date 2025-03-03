e57b9a939549f043a72d456b1ba1b4c50fd19879
Stephen <sokhrannykhs@gmail.com>
2025-03-03 22:00:08 +0500

    feat: add product type configuration

---
+++ EntityClasses/Configurations/ProductTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EntityClasses.Sales;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClasses.Configurations;
+
+internal class ProductTypeConfiguration: IEntityTypeConfiguration<ProductType>
+{
+    public void Configure(EntityTypeBuilder<ProductType> builder)
+    {
+        builder.HasKey(pt => pt.Id);
+
+        builder.Property(pt => pt.Id).HasColumnName("Id");
+
+        builder.HasIndex(pt => pt.Name).IsUnique();
+
+        builder.Property(pt => pt.Name).IsRequired().HasMaxLength(50);
+    }
+}
