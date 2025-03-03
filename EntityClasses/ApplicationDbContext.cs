378b19b7608ebf9dc6975638eca0c77098c31d9a
Stephen <sokhrannykhs@gmail.com>
2025-03-03 22:13:16 +0500

    chore: wiped excessive dependencies

--- EntityClasses/ApplicationDbContext.cs
+++ EntityClasses/ApplicationDbContext.cs
@@ -2,11 +2,6 @@
 using EntityClasses.Sales;
 using EntityClasses.Tournaments;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntityClasses;
 
