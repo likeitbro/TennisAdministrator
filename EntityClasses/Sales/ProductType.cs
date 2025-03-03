9ed7d8a37f0f54dd09d4c4f5c428395b777922bb
Stephen <sokhrannykhs@gmail.com>
2025-03-03 15:14:43 +0500

    feat: product type class added

---
+++ EntityClasses/Sales/ProductType.cs
@@ -0,0 +1,35 @@
+﻿using EntityClasses.Abstractions;
+using Throw;
+
+namespace EntityClasses.Sales;
+
+public class ProductType : Entity<ProductType>
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw()
+                .IfNullOrWhiteSpace(n => n);
+        }
+    }
+
+    protected ProductType(string name)
+        : base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static ProductType Create(string name)
+    {
+        return new(name);
+    }
+
+    public void Update(string name)
+    {
+        Name = name;
+    }
+}
