8ec90ae855a4850cbb8b55fd442f1db12a59b6d9
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:10:04 +0500

    feat: add tournament attendee config

---
+++ EntityClasses/Configurations/TournamentAttendeeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EntityClasses.Tournaments;
+using EntityClasses.Person;
+
+namespace EntityClasses.Configurations;
+
+public class TournamentAttendeeConfiguration : IEntityTypeConfiguration<TournamentAttendee>
+{
+    public void Configure(EntityTypeBuilder<TournamentAttendee> builder)
+    {
+        builder.HasKey(ta => ta.Id);
+
+        builder.Property(ta => ta.ClientId).IsRequired();
+
+        builder.Property(ta => ta.TournamentId).IsRequired();
+
+        builder.HasOne<Client>(ta => ta.Attendee)
+            .WithMany(a => a.Attends)
+            .HasForeignKey(ta => ta.ClientId)
+            .IsRequired();
+
+        builder.HasOne<Tournament>(ta => ta.Tournament)
+            .WithMany(t => t.TournamentAttendees)
+            .HasForeignKey(ta => ta.TournamentId)
+            .IsRequired();
+    }
+}
