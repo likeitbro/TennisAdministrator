7ecd95a547527d034c40dd5483333fad92eb3f8c
Stephen <sokhrannykhs@gmail.com>
2025-03-07 09:51:02 +0500

    feat: add sale config

---
+++ EntityClasses/Configurations/SaleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EntityClasses.Sales;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EntityClasses.Person;
+
+namespace EntityClasses.Configurations;
+
+public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+{
+    public void Configure(EntityTypeBuilder<Sale> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.ClientId).IsRequired();
+
+        builder.Property(s => s.SaleTime).IsRequired();
+
+        builder.Property(s => s.Revenue).IsRequired();
+
+        builder.HasOne<Client>(s => s.Client)
+            .WithMany(c => c.Sales)
+            .HasForeignKey(s => s.ClientId)
+            .IsRequired();
+    }
+}
