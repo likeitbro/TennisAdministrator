404aef0f1054ed1961653d3d326012ef8c4e04e6
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:43:42 +0500

    feat: add trainer type config

---
+++ EntityClasses/Configurations/TrainerTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EntityClasses.Person;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityClasses.Configurations;
+
+public class TrainerTypeConfiguration : IEntityTypeConfiguration<TrainerType>
+{
+    public void Configure(EntityTypeBuilder<TrainerType> builder)
+    {
+        builder.HasKey(tt => tt.Id);
+
+        builder.Property(tt => tt.Id).HasColumnName("Id");
+
+        builder.HasIndex(tt => tt.Name).IsUnique();
+
+        builder.Property(tt => tt.Name).IsRequired().HasMaxLength(50);
+    }
+}
