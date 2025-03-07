307e78d09ecc8ce781aa6b61e6e8f0d059b67946
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:31:53 +0500

    feat: add specialization config

---
+++ EntityClasses/Configurations/SpecializationConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EntityClasses.Person;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityClasses.Configurations;
+
+public class SpecializationConfiguration : IEntityTypeConfiguration<Specialization>
+{
+    public void Configure(EntityTypeBuilder<Specialization> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.SportId).IsRequired();
+
+        builder.Property(s => s.TrainerId).IsRequired();
+
+        builder.HasOne<Sport>(s => s.Sport)
+            .WithMany(s => s.Specializations)
+            .HasForeignKey(s => s.SportId)
+            .IsRequired();
+
+        builder.HasOne<Trainer>(s => s.Trainer)
+            .WithMany(t => t.Specializations)
+            .HasForeignKey(s => s.TrainerId)
+            .IsRequired();
+    }
+}
