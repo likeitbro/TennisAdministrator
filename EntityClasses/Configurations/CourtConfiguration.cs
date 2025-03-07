50b4a1b5893fe5ff3dcb5cc41d74199019046563
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:46:59 +0500

    feat: add court config

---
+++ EntityClasses/Configurations/CourtConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityClasses.Configurations;
+
+public class CourtConfiguration : IEntityTypeConfiguration<Court>
+{
+    public void Configure(EntityTypeBuilder<Court> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name).IsRequired().HasMaxLength(50);
+
+        builder.Property(c => c.Description).IsRequired().HasMaxLength(500);
+
+        builder.Property(c => c.Price).IsRequired();
+    }
+}
