0c342dbc2f211268cdd685d88d5cd21f7b6b5a43
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:21:28 +0500

    feat: add sport config

---
+++ EntityClasses/Configurations/SportConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityClasses.Configurations;
+
+public class SportConfiguration : IEntityTypeConfiguration<Sport>
+{
+    public void Configure(EntityTypeBuilder<Sport> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Id).HasColumnName("Id");
+
+        builder.HasIndex(s => s.Name).IsUnique();
+
+        builder.Property(s => s.Name).IsRequired().HasMaxLength(50);
+    }
+}
