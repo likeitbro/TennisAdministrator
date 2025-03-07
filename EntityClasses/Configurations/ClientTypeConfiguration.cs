91022ff5f014df41c02c411d7fb7ffb2b5029174
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:00:02 +0500

    feat: add client type config

---
+++ EntityClasses/Configurations/ClientTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EntityClasses.Person;
+
+namespace EntityClasses.Configurations;
+
+public class ClientTypeConfiguration : IEntityTypeConfiguration<ClientType>
+{
+    public void Configure(EntityTypeBuilder<ClientType> builder)
+    {
+        builder.HasKey(ct => ct.Id);
+
+        builder.Property(ct => ct.Id).HasColumnName("Id");
+
+        builder.HasIndex(ct => ct.Name).IsUnique();
+
+        builder.Property(ct => ct.Name).IsRequired().HasMaxLength(50);
+    }
+}
