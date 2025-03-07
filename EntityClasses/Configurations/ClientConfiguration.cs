bc9a97126c03edc08c8df5c3490eec2d91d780ea
Stephen <sokhrannykhs@gmail.com>
2025-03-07 09:58:10 +0500

    feat: add client config

---
+++ EntityClasses/Configurations/ClientConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EntityClasses.Person;
+
+namespace EntityClasses.Configurations;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.FirstName).IsRequired().HasMaxLength(50);
+
+        builder.Property(c => c.LastName).IsRequired().HasMaxLength(50);
+
+        builder.Property(c => c.ClientTypeId).IsRequired();
+
+        builder.Property(c => c.Birthday).IsRequired();
+
+        builder.Property(c => c.Phone).IsRequired().HasMaxLength(15);
+
+        builder.HasOne<ClientType>(c => c.ClientType)
+            .WithMany(ct => ct.Clients)
+            .HasForeignKey(c => c.ClientTypeId)
+            .IsRequired();
+    }
+}
