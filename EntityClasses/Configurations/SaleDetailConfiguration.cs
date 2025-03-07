03b91a64161b3164425a2fb0958360013cc83682
Stephen <sokhrannykhs@gmail.com>
2025-03-07 09:46:28 +0500

    feat: add sale detail config

---
+++ EntityClasses/Configurations/SaleDetailConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EntityClasses.Sales;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityClasses.Configurations;
+
+public class SaleDetailConfiguration : IEntityTypeConfiguration<SaleDetail>
+{
+    public void Configure(EntityTypeBuilder<SaleDetail> builder)
+    {
+        builder.HasKey(sd => sd.Id);
+
+        builder.Property(sd => sd.ProductId).IsRequired();
+
+        builder.Property(sd => sd.SaleId).IsRequired();
+
+        builder.HasOne<Product>(sd => sd.Product)
+            .WithMany(p => p.SaleDetails)
+            .HasForeignKey(sd => sd.ProductId)
+            .IsRequired();
+
+        builder.HasOne<Sale>(sd => sd.Sale)
+            .WithMany(s => s.SaleDetails)
+            .HasForeignKey(sd => sd.SaleId)
+            .IsRequired();
+    }
+}
