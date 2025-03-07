746239d05cdadfd99dd6ac64016f3b24104198d5
Stephen <sokhrannykhs@gmail.com>
2025-03-07 10:18:19 +0500

    feat: add tournament config

---
+++ EntityClasses/Configurations/TournamentConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EntityClasses.Tournaments;
+
+namespace EntityClasses.Configurations;
+
+public class TournamentConfiguration : IEntityTypeConfiguration<Tournament>
+{
+    public void Configure(EntityTypeBuilder<Tournament> builder)
+    {
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Name).IsRequired().HasMaxLength(100);
+
+        builder.Property(t => t.SportId).IsRequired();
+
+        builder.Property(t => t.Price).IsRequired();
+
+        builder.Property(t => t.Date).IsRequired();
+
+        builder.Property(t => t.Length).IsRequired();
+
+        builder.Property(t => t.Slots).IsRequired();
+
+        builder.HasOne<Sport>(t => t.Sport)
+            .WithMany(s => s.Tournaments)
+            .HasForeignKey(t => t.SportId)
+            .IsRequired();
+    }
+}
