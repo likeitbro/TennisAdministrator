ebc90a49c672f72764122b1b3390df0293ee7de6
Stephen <sokhrannykhs@gmail.com>
2025-03-10 22:02:42 +0500

    feat: add tournament form

---
+++ TennisAdministrator/TournamentForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace TennisAdministrator
+{
+    public partial class TournamentForm : Form
+    {
+        public TournamentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form? mainForm = Application.OpenForms.Cast<Form>().ToList().Find(f => f.Name == "MainForm");
+            if (mainForm != null)
+                mainForm.Activate();
+            this.Close();
+        }
+    }
+}
