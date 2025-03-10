93b6269d811d0d9ae41490900228bd9af543dedf
Stephen <sokhrannykhs@gmail.com>
2025-03-10 20:18:43 +0500

    feat: layout changed for usability

--- TennisAdministrator/MainForm.Designer.cs
+++ TennisAdministrator/MainForm.Designer.cs
@@ -47,7 +47,7 @@
             labelHeader.Font = new Font("Segoe UI", 32F, FontStyle.Bold);
             labelHeader.Location = new Point(0, 0);
             labelHeader.Name = "labelHeader";
-            labelHeader.Size = new Size(1264, 106);
+            labelHeader.Size = new Size(804, 73);
             labelHeader.TabIndex = 0;
             labelHeader.Text = "Теннисный клуб \"MAX CLUB\"";
             labelHeader.TextAlign = ContentAlignment.MiddleCenter;
@@ -68,33 +68,34 @@
             tableLayoutPanel1.Controls.Add(button7, 2, 1);
             tableLayoutPanel1.Controls.Add(button8, 3, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 106);
+            tableLayoutPanel1.Location = new Point(0, 73);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.Padding = new Padding(50);
+            tableLayoutPanel1.Padding = new Padding(20);
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(1264, 575);
+            tableLayoutPanel1.Size = new Size(804, 248);
             tableLayoutPanel1.TabIndex = 1;
             // 
             // button1
             // 
             button1.Dock = DockStyle.Fill;
             button1.Font = new Font("Segoe UI", 18F);
-            button1.Location = new Point(53, 53);
+            button1.Location = new Point(23, 23);
             button1.Name = "button1";
-            button1.Size = new Size(285, 231);
+            button1.Size = new Size(185, 98);
             button1.TabIndex = 0;
             button1.Text = "Корты";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
             button2.Dock = DockStyle.Fill;
             button2.Font = new Font("Segoe UI", 18F);
-            button2.Location = new Point(344, 53);
+            button2.Location = new Point(214, 23);
             button2.Name = "button2";
-            button2.Size = new Size(285, 231);
+            button2.Size = new Size(185, 98);
             button2.TabIndex = 1;
             button2.Text = "Бронирования";
             button2.UseVisualStyleBackColor = true;
@@ -103,9 +104,9 @@
             // 
             button3.Dock = DockStyle.Fill;
             button3.Font = new Font("Segoe UI", 18F);
-            button3.Location = new Point(635, 53);
+            button3.Location = new Point(405, 23);
             button3.Name = "button3";
-            button3.Size = new Size(285, 231);
+            button3.Size = new Size(185, 98);
             button3.TabIndex = 2;
             button3.Text = "Продажи";
             button3.UseVisualStyleBackColor = true;
@@ -114,9 +115,9 @@
             // 
             button4.Dock = DockStyle.Fill;
             button4.Font = new Font("Segoe UI", 18F);
-            button4.Location = new Point(926, 53);
+            button4.Location = new Point(596, 23);
             button4.Name = "button4";
-            button4.Size = new Size(285, 231);
+            button4.Size = new Size(185, 98);
             button4.TabIndex = 3;
             button4.Text = "Товары";
             button4.UseVisualStyleBackColor = true;
@@ -125,9 +126,9 @@
             // 
             button5.Dock = DockStyle.Fill;
             button5.Font = new Font("Segoe UI", 18F);
-            button5.Location = new Point(53, 290);
+            button5.Location = new Point(23, 127);
             button5.Name = "button5";
-            button5.Size = new Size(285, 232);
+            button5.Size = new Size(185, 98);
             button5.TabIndex = 4;
             button5.Text = "Клиенты";
             button5.UseVisualStyleBackColor = true;
@@ -136,9 +137,9 @@
             // 
             button6.Dock = DockStyle.Fill;
             button6.Font = new Font("Segoe UI", 18F);
-            button6.Location = new Point(344, 290);
+            button6.Location = new Point(214, 127);
             button6.Name = "button6";
-            button6.Size = new Size(285, 232);
+            button6.Size = new Size(185, 98);
             button6.TabIndex = 5;
             button6.Text = "Тренеры";
             button6.UseVisualStyleBackColor = true;
@@ -147,9 +148,9 @@
             // 
             button7.Dock = DockStyle.Fill;
             button7.Font = new Font("Segoe UI", 18F);
-            button7.Location = new Point(635, 290);
+            button7.Location = new Point(405, 127);
             button7.Name = "button7";
-            button7.Size = new Size(285, 232);
+            button7.Size = new Size(185, 98);
             button7.TabIndex = 6;
             button7.Text = "Турниры";
             button7.UseVisualStyleBackColor = true;
@@ -158,9 +159,9 @@
             // 
             button8.Dock = DockStyle.Fill;
             button8.Font = new Font("Segoe UI", 18F);
-            button8.Location = new Point(926, 290);
+            button8.Location = new Point(596, 127);
             button8.Name = "button8";
-            button8.Size = new Size(285, 232);
+            button8.Size = new Size(185, 98);
             button8.TabIndex = 7;
             button8.Text = "Спорты";
             button8.UseVisualStyleBackColor = true;
@@ -169,12 +170,13 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1264, 681);
+            ClientSize = new Size(804, 321);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(labelHeader);
-            MinimumSize = new Size(1280, 720);
+            MinimumSize = new Size(820, 360);
             Name = "MainForm";
             ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "НСИ Теннисного Клуба";
             tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
