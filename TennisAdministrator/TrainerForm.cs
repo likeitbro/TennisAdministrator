fb2b24850bd03800ab8bd38db0ce8c0edba21307
Stephen <sokhrannykhs@gmail.com>
2025-03-10 22:02:53 +0500

    feat: add trainer form

---
+++ TennisAdministrator/TrainerForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace TennisAdministrator
+{
+    public partial class TrainerForm : Form
+    {
+        public TrainerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form? mainForm = Application.OpenForms.Cast<Form>().ToList().Find(f => f.Name == "MainForm");
+            if (mainForm != null)
+                mainForm.Activate();
+            this.Close();
+        }
+    }
+}
