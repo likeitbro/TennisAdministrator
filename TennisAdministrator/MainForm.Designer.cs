3e8c6e47997e47bc929ab588bbf7207cdbafd8f2
Stephen <sokhrannykhs@gmail.com>
2025-03-10 18:43:19 +0500

    feat: main form layout

--- TennisAdministrator/MainForm.Designer.cs
+++ TennisAdministrator/MainForm.Designer.cs
@@ -28,20 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelHeader = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            labelHeader.Dock = DockStyle.Top;
+            labelHeader.Font = new Font("Segoe UI", 32F, FontStyle.Bold);
+            labelHeader.Location = new Point(0, 0);
+            labelHeader.Name = "labelHeader";
+            labelHeader.Size = new Size(1264, 106);
+            labelHeader.TabIndex = 0;
+            labelHeader.Text = "Теннисный клуб \"MAX CLUB\"";
+            labelHeader.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Controls.Add(button3, 2, 0);
+            tableLayoutPanel1.Controls.Add(button4, 3, 0);
+            tableLayoutPanel1.Controls.Add(button5, 0, 1);
+            tableLayoutPanel1.Controls.Add(button6, 1, 1);
+            tableLayoutPanel1.Controls.Add(button7, 2, 1);
+            tableLayoutPanel1.Controls.Add(button8, 3, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 106);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(50);
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1264, 575);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Font = new Font("Segoe UI", 18F);
+            button1.Location = new Point(53, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(285, 231);
+            button1.TabIndex = 0;
+            button1.Text = "Корты";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Segoe UI", 18F);
+            button2.Location = new Point(344, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(285, 231);
+            button2.TabIndex = 1;
+            button2.Text = "Бронирования";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Segoe UI", 18F);
+            button3.Location = new Point(635, 53);
+            button3.Name = "button3";
+            button3.Size = new Size(285, 231);
+            button3.TabIndex = 2;
+            button3.Text = "Продажи";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Fill;
+            button4.Font = new Font("Segoe UI", 18F);
+            button4.Location = new Point(926, 53);
+            button4.Name = "button4";
+            button4.Size = new Size(285, 231);
+            button4.TabIndex = 3;
+            button4.Text = "Товары";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Fill;
+            button5.Font = new Font("Segoe UI", 18F);
+            button5.Location = new Point(53, 290);
+            button5.Name = "button5";
+            button5.Size = new Size(285, 232);
+            button5.TabIndex = 4;
+            button5.Text = "Клиенты";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Dock = DockStyle.Fill;
+            button6.Font = new Font("Segoe UI", 18F);
+            button6.Location = new Point(344, 290);
+            button6.Name = "button6";
+            button6.Size = new Size(285, 232);
+            button6.TabIndex = 5;
+            button6.Text = "Тренеры";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Dock = DockStyle.Fill;
+            button7.Font = new Font("Segoe UI", 18F);
+            button7.Location = new Point(635, 290);
+            button7.Name = "button7";
+            button7.Size = new Size(285, 232);
+            button7.TabIndex = 6;
+            button7.Text = "Турниры";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Dock = DockStyle.Fill;
+            button8.Font = new Font("Segoe UI", 18F);
+            button8.Location = new Point(926, 290);
+            button8.Name = "button8";
+            button8.Size = new Size(285, 232);
+            button8.TabIndex = 7;
+            button8.Text = "Спорты";
+            button8.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1264, 681);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(labelHeader);
             MinimumSize = new Size(1280, 720);
             Name = "MainForm";
             ShowIcon = false;
             Text = "НСИ Теннисного Клуба";
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label labelHeader;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
