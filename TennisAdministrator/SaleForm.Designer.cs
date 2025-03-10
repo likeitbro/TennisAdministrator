e4728fea043ae5df99611745c54eee9f2252dafd
Stephen <sokhrannykhs@gmail.com>
2025-03-10 21:45:02 +0500

    feat: add sale form

---
+++ TennisAdministrator/SaleForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TennisAdministrator
+{
+    partial class SaleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(dataGridView1, 0, 2);
+            tableLayoutPanel1.Controls.Add(button1, 2, 1);
+            tableLayoutPanel1.Controls.Add(button2, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(1264, 681);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(label1, 3);
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(1258, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Список продаж";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableLayoutPanel1.SetColumnSpan(dataGridView1, 3);
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 83);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1258, 595);
+            dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = Properties.Resources.icons8_add_48;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(1227, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(34, 34);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Segoe UI", 14F);
+            button2.Image = Properties.Resources.icons8_go_back_24;
+            button2.ImageAlign = ContentAlignment.MiddleRight;
+            button2.Location = new Point(3, 43);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 34);
+            button2.TabIndex = 3;
+            button2.Text = "В меню";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // SaleForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(640, 360);
+            Name = "SaleForm";
+            ShowIcon = false;
+            Text = "Список продаж";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+    }
+}
