de342c4f5c45c63fc2994608c0cccdfca9ccc7c7
Stephen <sokhrannykhs@gmail.com>
2025-03-11 15:04:06 +0500

    feat: add list to data table converter using FastMember

---
+++ TennisAdministrator/ListToDataTable.cs
@@ -0,0 +1,17 @@
+﻿using FastMember;
+using System.Data;
+
+namespace TennisAdministrator;
+
+public static class ListToDataTable
+{
+    public static DataTable Convert<T>(List<T> data)
+    {
+        DataTable table = new DataTable();
+        using (var reader = ObjectReader.Create(data))
+        {
+            table.Load(reader);
+        }
+        return table;
+    }
+}
