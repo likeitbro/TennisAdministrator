ebd3dda95c6c9eaabbb7737814937891d03b3411
Stephen <sokhrannykhs@gmail.com>
2025-03-14 23:24:43 +0500

    feat: dialog for adding/updating trainer and linked specializations

---
+++ TennisAdministrator/TrainerCreateDialogForm.Designer.cs
@@ -0,0 +1,462 @@
+﻿namespace TennisAdministrator
+{
+    partial class TrainerCreateDialogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            errorLabel1 = new Label();
+            errorLabel2 = new Label();
+            errorLabel3 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            comboBox1 = new ComboBox();
+            errorLabel4 = new Label();
+            errorLabel5 = new Label();
+            errorLabel6 = new Label();
+            errorLabel7 = new Label();
+            button1 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            label10 = new Label();
+            errorLabel8 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 75F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 3);
+            tableLayoutPanel1.Controls.Add(label4, 0, 5);
+            tableLayoutPanel1.Controls.Add(textBox1, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBox2, 1, 3);
+            tableLayoutPanel1.Controls.Add(textBox3, 1, 5);
+            tableLayoutPanel1.Controls.Add(errorLabel1, 1, 2);
+            tableLayoutPanel1.Controls.Add(errorLabel2, 1, 4);
+            tableLayoutPanel1.Controls.Add(errorLabel3, 1, 6);
+            tableLayoutPanel1.Controls.Add(label5, 0, 7);
+            tableLayoutPanel1.Controls.Add(label6, 0, 9);
+            tableLayoutPanel1.Controls.Add(label7, 0, 11);
+            tableLayoutPanel1.Controls.Add(label8, 0, 13);
+            tableLayoutPanel1.Controls.Add(label9, 0, 15);
+            tableLayoutPanel1.Controls.Add(textBox4, 1, 7);
+            tableLayoutPanel1.Controls.Add(textBox5, 1, 9);
+            tableLayoutPanel1.Controls.Add(textBox6, 1, 11);
+            tableLayoutPanel1.Controls.Add(textBox7, 1, 13);
+            tableLayoutPanel1.Controls.Add(comboBox1, 1, 15);
+            tableLayoutPanel1.Controls.Add(errorLabel4, 1, 8);
+            tableLayoutPanel1.Controls.Add(errorLabel5, 1, 10);
+            tableLayoutPanel1.Controls.Add(errorLabel6, 1, 12);
+            tableLayoutPanel1.Controls.Add(errorLabel7, 1, 14);
+            tableLayoutPanel1.Controls.Add(button1, 1, 18);
+            tableLayoutPanel1.Controls.Add(checkedListBox1, 1, 17);
+            tableLayoutPanel1.Controls.Add(label10, 0, 17);
+            tableLayoutPanel1.Controls.Add(errorLabel8, 1, 16);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 19;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.Size = new Size(468, 511);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(label1, 2);
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(462, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Добавить нового тренера";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Имя";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Location = new Point(3, 75);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Фамилия";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Location = new Point(3, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 30);
+            label4.TabIndex = 3;
+            label4.Text = "Описание";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(120, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(345, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Fill;
+            textBox2.Location = new Point(120, 78);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(345, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Dock = DockStyle.Fill;
+            textBox3.Location = new Point(120, 123);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(345, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // errorLabel1
+            // 
+            errorLabel1.AutoSize = true;
+            errorLabel1.Dock = DockStyle.Fill;
+            errorLabel1.ForeColor = Color.Red;
+            errorLabel1.Location = new Point(120, 60);
+            errorLabel1.Name = "errorLabel1";
+            errorLabel1.Size = new Size(345, 15);
+            errorLabel1.TabIndex = 8;
+            errorLabel1.Text = "errorLabel1";
+            errorLabel1.Visible = false;
+            // 
+            // errorLabel2
+            // 
+            errorLabel2.AutoSize = true;
+            errorLabel2.Dock = DockStyle.Fill;
+            errorLabel2.ForeColor = Color.Red;
+            errorLabel2.Location = new Point(120, 105);
+            errorLabel2.Name = "errorLabel2";
+            errorLabel2.Size = new Size(345, 15);
+            errorLabel2.TabIndex = 9;
+            errorLabel2.Text = "errorLabel2";
+            errorLabel2.Visible = false;
+            // 
+            // errorLabel3
+            // 
+            errorLabel3.AutoSize = true;
+            errorLabel3.Dock = DockStyle.Fill;
+            errorLabel3.ForeColor = Color.Red;
+            errorLabel3.Location = new Point(120, 150);
+            errorLabel3.Name = "errorLabel3";
+            errorLabel3.Size = new Size(345, 15);
+            errorLabel3.TabIndex = 10;
+            errorLabel3.Text = "errorLabel3";
+            errorLabel3.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Location = new Point(3, 165);
+            label5.Name = "label5";
+            label5.Size = new Size(111, 30);
+            label5.TabIndex = 12;
+            label5.Text = "Дата рождения";
+            label5.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Location = new Point(3, 210);
+            label6.Name = "label6";
+            label6.Size = new Size(111, 30);
+            label6.TabIndex = 13;
+            label6.Text = "Телефон";
+            label6.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Fill;
+            label7.Location = new Point(3, 255);
+            label7.Name = "label7";
+            label7.Size = new Size(111, 30);
+            label7.TabIndex = 14;
+            label7.Text = "Опыт работы с";
+            label7.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Dock = DockStyle.Fill;
+            label8.Location = new Point(3, 300);
+            label8.Name = "label8";
+            label8.Size = new Size(111, 30);
+            label8.TabIndex = 15;
+            label8.Text = "Стоимость/час";
+            label8.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Dock = DockStyle.Fill;
+            label9.Location = new Point(3, 345);
+            label9.Name = "label9";
+            label9.Size = new Size(111, 30);
+            label9.TabIndex = 16;
+            label9.Text = "Категория";
+            label9.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // textBox4
+            // 
+            textBox4.Dock = DockStyle.Fill;
+            textBox4.Location = new Point(120, 168);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(345, 23);
+            textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            textBox5.Dock = DockStyle.Fill;
+            textBox5.Location = new Point(120, 213);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(345, 23);
+            textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            textBox6.Dock = DockStyle.Fill;
+            textBox6.Location = new Point(120, 258);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(345, 23);
+            textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            textBox7.Dock = DockStyle.Fill;
+            textBox7.Location = new Point(120, 303);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(345, 23);
+            textBox7.TabIndex = 20;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Fill;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(120, 348);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(345, 23);
+            comboBox1.TabIndex = 21;
+            // 
+            // errorLabel4
+            // 
+            errorLabel4.AutoSize = true;
+            errorLabel4.ForeColor = Color.Red;
+            errorLabel4.Location = new Point(120, 195);
+            errorLabel4.Name = "errorLabel4";
+            errorLabel4.Size = new Size(66, 15);
+            errorLabel4.TabIndex = 22;
+            errorLabel4.Text = "errorLabel4";
+            errorLabel4.Visible = false;
+            // 
+            // errorLabel5
+            // 
+            errorLabel5.AutoSize = true;
+            errorLabel5.ForeColor = Color.Red;
+            errorLabel5.Location = new Point(120, 240);
+            errorLabel5.Name = "errorLabel5";
+            errorLabel5.Size = new Size(66, 15);
+            errorLabel5.TabIndex = 23;
+            errorLabel5.Text = "errorLabel5";
+            errorLabel5.Visible = false;
+            // 
+            // errorLabel6
+            // 
+            errorLabel6.AutoSize = true;
+            errorLabel6.ForeColor = Color.Red;
+            errorLabel6.Location = new Point(120, 285);
+            errorLabel6.Name = "errorLabel6";
+            errorLabel6.Size = new Size(66, 15);
+            errorLabel6.TabIndex = 24;
+            errorLabel6.Text = "errorLabel6";
+            errorLabel6.Visible = false;
+            // 
+            // errorLabel7
+            // 
+            errorLabel7.AutoSize = true;
+            errorLabel7.ForeColor = Color.Red;
+            errorLabel7.Location = new Point(120, 330);
+            errorLabel7.Name = "errorLabel7";
+            errorLabel7.Size = new Size(66, 15);
+            errorLabel7.TabIndex = 25;
+            errorLabel7.Text = "errorLabel7";
+            errorLabel7.Visible = false;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Right;
+            button1.Location = new Point(365, 483);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 25);
+            button1.TabIndex = 26;
+            button1.Text = "Сохранить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.Dock = DockStyle.Fill;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(120, 393);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(345, 84);
+            checkedListBox1.TabIndex = 27;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Dock = DockStyle.Fill;
+            label10.Location = new Point(3, 390);
+            label10.Name = "label10";
+            label10.Size = new Size(111, 90);
+            label10.TabIndex = 28;
+            label10.Text = "Спорт";
+            label10.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // errorLabel8
+            // 
+            errorLabel8.AutoSize = true;
+            errorLabel8.ForeColor = Color.Red;
+            errorLabel8.Location = new Point(120, 375);
+            errorLabel8.Name = "errorLabel8";
+            errorLabel8.Size = new Size(66, 15);
+            errorLabel8.TabIndex = 29;
+            errorLabel8.Text = "errorLabel8";
+            errorLabel8.Visible = false;
+            // 
+            // TrainerCreateDialogForm
+            // 
+            AcceptButton = button1;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(468, 511);
+            Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TrainerCreateDialogForm";
+            ShowIcon = false;
+            Text = "Изменениие данных о тренерах";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label errorLabel1;
+        private Label errorLabel2;
+        private Label errorLabel3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private ComboBox comboBox1;
+        private Label errorLabel4;
+        private Label errorLabel5;
+        private Label errorLabel6;
+        private Label errorLabel7;
+        private Button button1;
+        private CheckedListBox checkedListBox1;
+        private Label label10;
+        private Label errorLabel8;
+    }
+}
---
+++ TennisAdministrator/TrainerCreateDialogForm.cs
@@ -0,0 +1,213 @@
+﻿using EntityClasses;
+using EntityClasses.Person;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+
+namespace TennisAdministrator
+{
+    public partial class TrainerCreateDialogForm : Form
+    {
+        string errorText = "Введены некорректные данные";
+        string? firstName;
+        string? lastName;
+        string? description;
+        DateOnly birthday = DateOnly.MinValue;
+        string? phone;
+        DateOnly experience = DateOnly.MinValue;
+        float price = 0;
+        List<TrainerType> trainerTypes;
+        List<Specialization> specializations;
+        ApplicationDbContext _dbContext;
+        Trainer _trainer;
+
+        public TrainerCreateDialogForm(ApplicationDbContext dbContext, Trainer trainer = null)
+        {
+            InitializeComponent();
+            _dbContext = dbContext;
+            trainerTypes = dbContext.TrainerTypes.ToList();
+            foreach (var trainerType in trainerTypes)
+                comboBox1.Items.Add(trainerType.Name);
+            comboBox1.SelectedIndex = 1;
+            List<Sport> sports = dbContext.Sports.ToList();
+            foreach (var sport in sports)
+                checkedListBox1.Items.Add(sport.Name);
+            if (trainer is not null)
+            {
+                _trainer = trainer;
+                label1.Text = "Изменить данные о тренере";
+                textBox1.Text = trainer.FirstName;
+                textBox2.Text = trainer.LastName;
+                textBox3.Text = trainer.Description;
+                textBox4.Text = trainer.Birthday.ToString();
+                textBox5.Text = trainer.Phone;
+                textBox6.Text = trainer.Experience.ToString();
+                textBox7.Text = trainer.Price.ToString();
+                comboBox1.SelectedItem = trainer.TrainerType.Name;
+                foreach (var specialization in trainer.Specializations)
+                    checkedListBox1.SetItemChecked(checkedListBox1.Items.IndexOf(specialization.Sport.Name), true);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            bool success = true;
+            if (textBox1.Text.Length == 0 || textBox1.Text.Length > 50)
+            {
+                errorLabel1.Text = errorText;
+                errorLabel1.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel1.Visible = false;
+                firstName = textBox1.Text;
+            }
+
+            if (textBox2.Text.Length == 0 || textBox1.Text.Length > 50)
+            {
+                errorLabel2.Text = errorText;
+                errorLabel2.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel2.Visible = false;
+                lastName = textBox2.Text;
+            }
+
+            if (textBox3.Text.Length == 0 || textBox3.Text.Length > 100)
+            {
+                errorLabel3.Text = errorText;
+                errorLabel3.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel3.Visible = false;
+                description = textBox3.Text;
+            }
+
+            if (!DateOnly.TryParse(textBox4.Text, out birthday) || birthday == DateOnly.MinValue)
+            {
+                errorLabel4.Text = errorText;
+                errorLabel4.Visible = true;
+                birthday = DateOnly.MinValue;
+                success = false;
+            }
+            else
+            {
+                errorLabel4.Visible = false;
+            }
+
+            if (textBox5.Text.Length == 0 || 
+                textBox5.Text.Length > 15 || 
+                !System.Text.RegularExpressions.Regex.IsMatch(textBox5.Text, @"^[0-9]*$"))
+            {
+                errorLabel5.Text = errorText;
+                errorLabel5.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel5.Visible = false;
+                phone = textBox5.Text;
+            }
+
+            if (!DateOnly.TryParse(textBox6.Text, out experience) || experience == DateOnly.MinValue)
+            {
+                errorLabel6.Text = errorText;
+                errorLabel6.Visible = true;
+                experience = DateOnly.MinValue;
+                success = false;
+            }
+            else
+            {
+                errorLabel6.Visible = false;
+            }
+
+            if (!float.TryParse(textBox7.Text, out price) || price <= 0)
+            {
+                errorLabel7.Text = errorText;
+                errorLabel7.Visible = true;
+                price = 0;
+                success = false;
+            }
+            else
+            {
+                errorLabel7.Visible = false;
+            }
+
+            if (checkedListBox1.CheckedItems.Count == 0)
+            {
+                errorLabel8.Text = "Выберите хотя бы один спорт";
+                errorLabel8.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel8.Visible = false;
+            }
+
+            return success;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                if (_trainer is not null)
+                {
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    {
+                        string item = checkedListBox1.GetItemText(i);
+                        if (checkedListBox1.GetItemChecked(i))
+                        {
+                            if (_trainer.Specializations.Where(s => s.Sport.Name == item).IsNullOrEmpty())
+                                _dbContext.Specializations.Add(Specialization.Create(
+                                    _trainer.Id,
+                                    _dbContext.Sports.Where(s => s.Name == item).First().Id));
+                        }
+                        else
+                        {
+                            if (_trainer.Specializations.Where(s => s.Sport.Name == item).IsNullOrEmpty())
+                                _dbContext.Specializations.Remove(
+                                    _dbContext.Specializations.Where(
+                                        s => s.Sport.Name == item && s.TrainerId == _trainer.Id).First());
+                        }
+                    }
+                    _trainer.Update(
+                        _dbContext.TrainerTypes.Where(tt => tt.Name == comboBox1.SelectedItem.ToString()).First().Id,
+                        firstName,
+                        lastName,
+                        birthday,
+                        phone,
+                        experience,
+                        description,
+                        price);
+
+                }
+                else
+                {
+                    _trainer = Trainer.Create(
+                        _dbContext.TrainerTypes.Where(tt => tt.Name == comboBox1.SelectedItem.ToString()).First().Id,
+                        firstName,
+                        lastName,
+                        birthday,
+                        phone,
+                        experience,
+                        description,
+                        price);
+                    _dbContext.Trainers.Add(_trainer);
+                    foreach (var item in checkedListBox1.CheckedItems.Cast<String>())
+                    {
+                        _dbContext.Specializations.Add(Specialization.Create(
+                                    _trainer.Id,
+                                    _dbContext.Sports.Where(s => s.Name == item).First().Id));
+                    }
+                }
+                _dbContext.SaveChanges();
+                this.Close();
+            }
+        }
+    }
+}
