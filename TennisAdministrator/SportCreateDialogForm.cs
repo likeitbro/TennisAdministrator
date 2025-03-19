04801431f4f1d7508fb932e22bd8dbe5a1c2c96a
Stephen <sokhrannykhs@gmail.com>
2025-03-19 21:39:42 +0500

    feat: sport form

---
+++ TennisAdministrator/SportCreateDialogForm.cs
@@ -0,0 +1,58 @@
+﻿using EntityClasses;
+
+namespace TennisAdministrator
+{
+    public partial class SportCreateDialogForm : Form
+    {
+        string errorText = "Введены некорректные данные";
+        string? name;
+        ApplicationDbContext _dbContext;
+        Sport _sport;
+
+        public SportCreateDialogForm(ApplicationDbContext dbContext, Sport sport = null)
+        {
+            InitializeComponent();
+            _dbContext = dbContext;
+            if (sport is not null)
+            {
+                _sport = sport;
+                label1.Text = "Изменить данные о типе тренеров";
+                textBox1.Text = sport.Name;
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            bool success = true;
+            if (textBox1.Text.Length == 0 || textBox1.Text.Length > 50)
+            {
+                errorLabel1.Text = errorText;
+                errorLabel1.Visible = true;
+                success = false;
+            }
+            else
+            {
+                errorLabel1.Visible = false;
+                name = textBox1.Text;
+            }
+
+            return success;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                if (_sport is not null)
+                    _sport.Update(name);
+                else
+                {
+                    _sport = Sport.Create(name);
+                    _dbContext.Sports.Add(_sport);
+                }
+                _dbContext.SaveChanges();
+                this.Close();
+            }
+        }
+    }
+}
--- TennisAdministrator/SportForm.cs
+++ TennisAdministrator/SportForm.cs
@@ -1,12 +1,51 @@
-﻿using System.Data;
+﻿using EntityClasses;
+using System.ComponentModel;
+using System.Data;
 
 namespace TennisAdministrator
 {
     public partial class SportForm : Form
     {
+        ApplicationDbContext dbContext;
+        int activeCellIndex = 0;
+
         public SportForm()
         {
             InitializeComponent();
+            dbContext = new ApplicationDbContext();
+            UpdateTable();
+            AdjustTable();
+            dataGridView1.Rows[activeCellIndex].DefaultCellStyle.BackColor = Color.LightGray;
+        }
+        private void UpdateTable()
+        {
+            List<Sport> sports = dbContext.Sports.ToList<Sport>();
+            dataGridView1.DataSource = ListToDataTable.Convert<Sport>(sports);
+            dataGridView1.Sort(dataGridView1.Columns["Name"], ListSortDirection.Ascending);
+        }
+
+        private void AdjustTable()
+        {
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.AllowUserToResizeColumns = true;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.Columns["Id"].Visible = false;
+
+            dataGridView1.Sort(dataGridView1.Columns["Name"], ListSortDirection.Ascending);
+
+            dataGridView1.Columns["Name"].HeaderText = "Название";
+
+            dataGridView1.Columns["Id"].DisplayIndex = 0;
+            dataGridView1.Columns["Name"].DisplayIndex = 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -15,6 +54,65 @@ namespace TennisAdministrator
             if (mainForm != null)
                 mainForm.Activate();
             this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SportCreateDialogForm addDialogForm = new SportCreateDialogForm(dbContext);
+            addDialogForm.ShowDialog();
+            UpdateTable();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if (e.RowIndex >= 0)
+            {
+                dataGridView1.Rows[activeCellIndex].DefaultCellStyle.BackColor = Color.White;
+                activeCellIndex = e.RowIndex;
+                dataGridView1.Rows[activeCellIndex].DefaultCellStyle.BackColor = Color.LightGray;
+                button3.Visible = true;
+                button4.Visible = true;
+            }
+            else
+            {
+                button3.Visible = false;
+                button4.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Вы уверены, что желаете удалить объект?",
+                                                    "Подтверждение удаления",
+                                                    MessageBoxButtons.OKCancel);
+            if (confirmResult == DialogResult.OK)
+            {
+                Guid id = Guid.Parse(dataGridView1.Rows[activeCellIndex].Cells["Id"].Value.ToString());
+                Sport? sport = dbContext.Sports.FirstOrDefault(s => s.Id == id);
+
+                if (sport is not null)
+                {
+                    dbContext.Sports.Remove(sport);
+                    dbContext.SaveChanges();
+                }
+                UpdateTable();
+                activeCellIndex = 0;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Guid id = Guid.Parse(dataGridView1.Rows[activeCellIndex].Cells["Id"].Value.ToString());
+            Sport? sport = dbContext.Sports.FirstOrDefault(s => s.Id == id);
+            SportCreateDialogForm addDialogForm = new SportCreateDialogForm(dbContext, sport);
+            addDialogForm.ShowDialog();
+            UpdateTable();
+        }
+
+        private void SportForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dbContext?.Dispose();
         }
     }
 }
